c0dca62257143df87ae08396b20f9880951f008e
Adrian Tillich <adrian.tillich@gmail.com>
2014-10-03 00:33:22 +0200

    some small refactorings to make code more concise

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -26,16 +26,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertButtonPressed(sender: UIButton) {
-        let sizeFromTextField = mensShoeSizeTextField.text
-        let numberFromTextField = sizeFromTextField.toInt()
-        var integerFromTextField = numberFromTextField!
-        let conversionConstant = 30
-        integerFromTextField += conversionConstant
-        
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
         mensConvertedShoeSizeLabel.hidden = false
-        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
-        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
-        
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + 30) in European Shoe Size"
         mensShoeSizeTextField.resignFirstResponder()
     }
 
